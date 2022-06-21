5e54a3dba244eca675f1be95d759a5d492aaaf23
Luan Ferreira Campos <lfcampos.eab@gmail.com>
2022-06-21 17:04:15 -0300

    loader

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import { Movie } from "./types/Movie";
 const App = () => {
 
   const [movies, setMovies] = useState<Movie[]>([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(()=> {
     handleLoad()
   },[])
 
   const handleLoad = async () => {
+    setLoading(true)
     let response = await fetch('https://api.b7web.com.br/cinema/')
     let json = await response.json()
-
     setMovies(json.reverse())
+    setLoading(false)
   }
 
   // const handleLoad = () => {
@@ -27,7 +29,25 @@ const App = () => {
   // }
 
   return (
+
     <div className="flex flex-col items-center justify-between gap-8 bg-indigo-800 min-h-screen text-indigo-300">
+      {loading &&
+        <div className="flex flex-col items-center justify-center gap-2 fixed z-10 w-screen h-screen bg-indigo-900">
+          <p>Carregando...</p>
+          <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            width="80px" height="80px" viewBox="0 0 50 50" xmlSpace="preserve">
+          <path fill="#a5b4fc" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">
+            <animateTransform attributeType="xml"
+              attributeName="transform"
+              type="rotate"
+              from="0 25 25"
+              to="360 25 25"
+              dur="0.6s"
+              repeatCount="indefinite"/>
+            </path>
+          </svg>
+        </div>
+      }
       <header id="header" className="flex flex-col items-center justify-center py-10 bg-indigo-900 w-full">
         <h1 className=" font-display text-6xl text-center">Filmes em Cartaz</h1>
         <h2 className="text-xl font-bold">Total de Filmes: {movies.length}</h2>
