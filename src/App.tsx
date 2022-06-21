235630b616e4902cd31b774adefd9bc60fffcd9d
Luan Ferreira Campos <lfcampos.eab@gmail.com>
2022-06-21 16:20:45 -0300

    layout updates

--- src/App.tsx
+++ src/App.tsx
@@ -27,20 +27,24 @@ const App = () => {
   // }
 
   return (
-    <div className="flex flex-col items-center justify-start gap-8 pb-10 bg-indigo-800 min-h-screen">
+    <div className="flex flex-col items-center justify-between gap-8 bg-indigo-800 min-h-screen text-indigo-300">
       <header id="header" className="flex flex-col items-center justify-center py-10 bg-indigo-900 w-full">
-        <h1 className=" font-display text-6xl text-white text-center">Filmes em Cartaz</h1>
-        <h2 className="text-xl text-white">Total de Filmes: {movies.length}</h2>
+        <h1 className=" font-display text-6xl text-center">Filmes em Cartaz</h1>
+        <h2 className="text-xl font-bold">Total de Filmes: {movies.length}</h2>
       </header>
 
       <section className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 max-w-7xl gap-y-10 gap-x-5 px-5">
         {movies.map((item,index)=>(
           <div>
             <img src={item.avatar} alt="" className="w-full rounded-md"/>
-            <p className="text-white text-center pt-1">{item.titulo}</p>
+            <p className="text-center pt-1 font-semibold">{item.titulo}</p>
           </div>
         ))}
       </section>
+
+      <footer className="bg-indigo-900 py-4 px-8 mt-10 w-full flex justify-center items-center">
+        <p className="max-w-sm text-center">Feito por <a className="font-bold text-indigo-100" href="https://github.com/luanfcampos">Luan Campos</a> durante o curso de desenvolvimento web fullstack da <a className="font-bold text-indigo-100" href="https://b7web.com.br/fullstack/">B7Web</a></p>
+      </footer>
     </div>
   );
 }
